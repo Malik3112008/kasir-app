623cef2e4834cfa6937bfa61c46bf00336ae58bf
raiylakee <mra.alfikri@gmail.com>
2026-06-03 20:22:00 +0700

    Remove leftover files and directories (Andin, noval, README, scripts, uv.lock)

--- run.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-# Script untuk menjalankan aplikasi kasir
-cd (dirname (status -f))
-./venv/bin/python app.py
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Script untuk menjalankan aplikasi kasir
-cd "$(dirname "$0")"
-./venv/bin/python app.py
